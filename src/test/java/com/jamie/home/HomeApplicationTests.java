7c0dbf866b1e3d323799f3d0de3d7bffef631faf
jamie <woqja0@gmail.com>
2022-07-18 19:01:44 +0900

    jamie update 2022.07.18

---
+++ src/test/java/com/jamie/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamie.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
