22656644fc0b9d3b946960805da77e6da979dcb9
jamie <woqja0@gmail.com>
2022-09-03 23:58:18 +0900

    jamie update 2022.09.03

--- src/main/java/com/jamie/home/api/service/BannerService.java
+++ src/main/java/com/jamie/home/api/service/BannerService.java
@@ -3,6 +3,7 @@ package com.jamie.home.api.service;
 import com.jamie.home.api.model.BANNER;
 import com.jamie.home.api.model.CATEGORY;
 import com.jamie.home.api.model.SEARCH;
+import com.jamie.home.util.FileUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,11 +20,28 @@ public class BannerService extends BasicService{
         return bannerDao.getListBannerCnt(search);
     }
 
-    public Integer save(BANNER banner) {
+    public Integer save(BANNER banner) throws Exception {
+        banner.setImage(
+                FileUtils.saveFiles(
+                        banner.getSaveFiles(),
+                        uploadDir
+                )
+        );
+        banner.setSaveFiles(null);
         return bannerDao.insertBanner(banner);
     }
 
-    public Integer modi(BANNER banner) {
+    public Integer modi(BANNER banner) throws Exception {
+        banner.setImage(
+                FileUtils.modiFiles(
+                        banner.getImage(),
+                        banner.getDeleteFiles(),
+                        banner.getSaveFiles(),
+                        uploadDir
+                )
+        );
+
+        banner.setSaveFiles(null);
         return bannerDao.updateBanner(banner);
     }
 
